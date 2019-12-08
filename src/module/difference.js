2929dfb636863919f82c5f532618c3e3536353af
Ashe3 <Korolewsky.anton3@gmail.com>
2019-12-08 21:23:15 +0200

    readme upd

--- src/module/difference.js
+++ src/module/difference.js
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import { has, flattenDeep, uniq } from 'lodash';
+import { has, uniq } from 'lodash';
 
 const parse = (filepath) => {
   const data = fs.readFileSync(filepath, 'utf-8');
@@ -13,13 +13,13 @@ const compareValues = (obj1, obj2, key) => {
   return [`+ ${key}:${obj2[key]}`, `- ${key}:${obj1[key]}`];
 };
 
-const formatOutput = (data) => `{\n${flattenDeep(data).join('\n')}\n}`;
+const format = (data) => `{\n${data.join('\n')}\n}`;
 
-const compareFilesData = (fileData1, fileData2) => {
+const generateDifference = (fileData1, fileData2) => {
   const keys = uniq([...Object.keys(fileData1), ...Object.keys(fileData2)]);
   const result = keys.reduce((acc, key) => {
     if (has(fileData1, key) && has(fileData2, key)) {
-      return [...acc, compareValues(fileData1, fileData2, key)];
+      return [...acc, ...compareValues(fileData1, fileData2, key)];
     }
     if (!has(fileData1, key) && has(fileData2, key)) {
       return [...acc, `+ ${key}: ${fileData2[key]}`];
@@ -35,5 +35,5 @@ const compareFilesData = (fileData1, fileData2) => {
 export default (filepath1, filepath2) => {
   const fileData1 = parse(filepath1);
   const fileData2 = parse(filepath2);
-  return formatOutput(compareFilesData(fileData1, fileData2));
+  return format(generateDifference(fileData1, fileData2));
 };
